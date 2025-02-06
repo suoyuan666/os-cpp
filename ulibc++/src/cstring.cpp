69bb87b0be034f6a55c82e3f87e0272d4c11680d
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2025-02-06 19:16:09 +0800

    [ulibc++]: add `strncpy` and `memmove`

--- ulibc++/src/cstring.cpp
+++ ulibc++/src/cstring.cpp
@@ -9,4 +9,26 @@ auto memset(void* dest, int ch, uint32_t count) -> void* {
   }
   return cdst;
 }
+
+auto memmove(void* dest, const void* src, uint32_t count) -> void* {
+  char* dst = static_cast<char*>(dest);
+  const auto* vsrc = (char*)src;
+  if (vsrc > dst) {
+    while (count-- > 0) *dst++ = *vsrc++;
+  } else {
+    dst += count;
+    vsrc += count;
+    while (count-- > 0) *--dst = *--vsrc;
+  }
+  return dest;
+}
+
+auto strncpy(char* __restrict__ dest, const char* __restrict__ src,
+             uint32_t count) -> char* {
+  auto* s = dest;
+  while (count-- > 0 && (*dest++ = *src++) != 0) {
+  };
+  while (count-- > 0) *dest++ = 0;
+  return s;
+}
 }  // namespace std
