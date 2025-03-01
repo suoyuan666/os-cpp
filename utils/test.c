98a6331355925881eb0d917e12f8583fc3f1a6f3
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2025-03-01 12:43:58 +0800

    [user]: improve test.c to make it more similar to init

--- utils/test.c
+++ utils/test.c
@@ -1,6 +1,16 @@
+#include <fnctl.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 int main() {
-  printf("hello, world\n");
+  if (open("console", O_RDWR) < 0) {
+    mknod("console", 1, 0);
+    open("console", O_RDWR);
+  }
+  dup(0);  // stdout
+  dup(0);  // stderr
+  write(1, "hello, world\n", 14);
   while (1) {
   };
   return 0;
