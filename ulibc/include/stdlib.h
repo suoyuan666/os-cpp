cb898ee5ac4e5e6e72427f38904939640f36f7cd
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2025-03-10 21:30:42 +0800

    [ulibc]: add malloc and free based on K&R malloc

--- ulibc/include/stdlib.h
+++ ulibc/include/stdlib.h
@@ -4,11 +4,16 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
 
 _Noreturn void exit (int);
 _Noreturn void _Exit (int);
+
+void *malloc (uint32_t);
+void free (void *);
 
 #ifdef __cplusplus
 }
