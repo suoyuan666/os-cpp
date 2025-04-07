bb7d632bce61bc1c7a2e838b3a7b1f757af81ea3
suoyuan666 <s5n666@outlook.com>
2025-04-07 20:28:50 +0800

    [utils]: fix init error
    Signed-off-by: suoyuan666 <s5n666@outlook.com>

--- utils/init.c
+++ utils/init.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main() {
-  char *argv[] = {"sh", 0};
+  char *argv[] = {"sh", nullptr};
 
   if (open("console", O_RDWR) < 0) {
     mknod("console", 1, 0);
@@ -13,8 +13,10 @@ int main() {
   dup(0);  // stdout
   dup(0);  // stderr
   printf("\ninit process start\n\n");
+  setuid(1000);
+  setgid(1000);
   if (fork() == 0) {
-    execve("sh", argv, NULL);
+    execve("/bin/sh", argv, nullptr);
   }
   while (1) {
   };
